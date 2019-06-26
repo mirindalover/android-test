5647ab1ff5f7beb5f9586fb5fc4e2523e6d286c9
mirindalover <1255756011@qq.com>
2019-06-26 18:00:11 +0800

    init

---
+++ app/src/main/java/com/mlc/android_test/MainActivity.java
@@ -0,0 +1,43 @@
+
+package com.mlc.android_test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private TextView mTextView;
+
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String text = mEditText.getText().toString();
+
+        final int changeTextBtId = R.id.changeTextBt;
+        final int activityChangeTextBtnId = R.id.activityChangeTextBtn;
+
+        if (view.getId() == changeTextBtId) {
+            mTextView.setText(text);
+        } else if (view.getId() == activityChangeTextBtnId) {
+            Intent intent = ShowTextActivity.newStartIntent(this, text);
+            startActivity(intent);
+        }
+    }
+}
